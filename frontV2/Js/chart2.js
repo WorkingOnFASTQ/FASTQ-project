e974b9786a5b0ef825de25ee7f84a6e5db8c45ab
WorkingOnFASTQ <105285646+WorkingOnFASTQ@users.noreply.github.com>
2022-05-10 13:21:55 +0200

    Add files via upload

---
+++ frontV2/Js/chart2.js
@@ -0,0 +1,30 @@
+// const ctx = document.getElementById('LineChart').getContext('2d');
+// const myChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['A', 'T', 'G', 'C', 'Purine', 'Pyrimidine'],
+//         datasets: [{
+//             label: '# of Votes',
+//              data: [1532, 1378, 829, 776, 2154, 2361],
+//             // data: [aCount, tCount, gCount, cCount, 2154, 2361],
+
+//             backgroundColor: [
+//                 'rgba(85, 85, 85, 1)'
+
+//             ],
+//             borderColor: [
+//                 'rgb(41, 155, 99, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         responsive:true
+//     }
+// });
+
+
+
+
+
+
